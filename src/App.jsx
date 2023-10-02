9388e2750b349e26e70c85c7bb10f575f0b4549a
f-rahim <freelancerrahim26@gmail.com>
2023-10-02 14:45:31 +0600

    Post URL data to database and retrive for show the UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import "./App.scss";
 import { RouterProvider } from "react-router-dom";
 import router from "./router/router";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <QueryClientProvider client={new QueryClient()}>
+        <RouterProvider router={router} />
+        <ToastContainer position="bottom-right" theme="dark" />
+      </QueryClientProvider>
+    </>
+  );
 }
 
 export default App;
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,24 +1,51 @@
+import axios from "axios";
 import { useState } from "react";
+import { useQuery } from "react-query";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
 import ShortUniqueId from "short-unique-id";
 const uid = new ShortUniqueId();
 
 const Main = () => {
   const [url, setUrl] = useState("");
-  const [urlDocument, setUrlDocument] = useState([]);
+
+  const {
+    data: allURL,
+    refetch,
+    isLoading,
+  } = useQuery("urlShorter", async () => {
+    return axios("http://localhost:5000/url")
+      .then((res) => res.data)
+      .catch((err) => console.error(err));
+  });
+
+  //   console.log(allURL);
 
   const handleShortURL = (e) => {
     e.preventDefault();
     const shortId = uid.rnd(5);
+    const time = new Date().getTime();
     const urlDoc = {
       shortId,
       url,
       clicks: 0,
+      time,
     };
 
-    setUrlDocument([urlDoc]);
-    setUrl("");
-
-    console.log("Short URL", urlDoc);
+    // console.log("Short URL", urlDoc);
+    axios
+      .post("http://localhost:5000/url", urlDoc)
+      .then((res) => {
+        const data = res.data;
+        if (data.acknowledged) {
+          refetch();
+          toast.success("URL Short sucessfuly 🚀");
+          setUrl("");
+        } else {
+          toast.info("Something is wrong! Please try again 🚀");
+        }
+      })
+      .catch((err) => console.error(err));
   };
 
   return (
@@ -48,6 +75,7 @@ const Main = () => {
                       className="w-full block col-span-2 border py-4 px-6"
                       onChange={(e) => setUrl(e.target.value)}
                       value={url}
+                      required
                     />
                     <button
                       className="w-full border border-blue-600 bg-blue-500 transition hover:bg-blue-600 text-white"
@@ -72,17 +100,24 @@ const Main = () => {
                   </tr>
                 </thead>
                 <tbody>
-                  {urlDocument.map((url) => (
-                    <tr key={url.shortId}>
-                      <td title={url.url}>{url.url.slice(0, 30)}...</td>
-                      <td>
-                        {url.shortId}{" "}
-                        <button className="float-right border px-2">
-                          Copy
-                        </button>
-                      </td>
-                    </tr>
-                  ))}
+                  {!isLoading &&
+                    allURL.map((url) => (
+                      <tr key={url.shortId}>
+                        <td title={url.url}>{url.url?.slice(0, 30)}...</td>
+                        <td>
+                          <Link
+                            to={`/${url.shortId}`}
+                            className="text-blue-500 font-semibold"
+                          >
+                            {url.shortId}
+                          </Link>{" "}
+                          <button className="float-right border px-2">
+                            Copy
+                          </button>
+                        </td>
+                        <td align="center">{url.clicks}</td>
+                      </tr>
+                    ))}
                 </tbody>
               </table>
             </div>
