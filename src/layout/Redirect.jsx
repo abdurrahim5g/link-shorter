85ae11988fb1c38aa90cafb6fafee3aa62fdb3e2
f-rahim <freelancerrahim26@gmail.com>
2023-10-02 15:12:54 +0600

    Redirect user to the longURL and track the user clicks

--- src/layout/Redirect.jsx
+++ src/layout/Redirect.jsx
@@ -1,7 +1,25 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import axios from "axios";
+import { useEffect } from "react";
+import { useLoaderData } from "react-router-dom";
+
 const Redirect = () => {
-  window.location.href =
-    "https://www.npmjs.com/package/short-unique-id?activeTab=readme";
-  return;
+  const data = useLoaderData().data[0];
+  const { url, shortId, clicks } = data;
+
+  useEffect(() => {
+    axios
+      .patch(`http://localhost:5000/url/${shortId}`, { clicks: clicks })
+      .then((res) => {
+        const data = res.data;
+        if (data.acknowledged && data.matchedCount) {
+          window.location.href = url;
+        }
+      });
+  }, []);
+  console.log(url, shortId);
+
+  return "redirecting...";
 };
 
 export default Redirect;
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,10 +1,16 @@
 import { Link, createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
 import Redirect from "../layout/Redirect";
+import axios from "axios";
 
 const router = createBrowserRouter([
   { path: "/", element: <Main /> },
-  { path: "/:id", element: <Redirect /> },
+  {
+    path: "/:id",
+    element: <Redirect />,
+    loader: ({ params }) =>
+      axios(`http://localhost:5000/url?shortId=${params.id}`),
+  },
   {
     path: "*",
     element: (
